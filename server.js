1755e9455b2f2a2fd9741592d8892c3bbb6dafa6
Alpha Bah <alphabah@Alphas-MacBook-Pro.local>
2021-07-02 22:12:44 -0600

    heroku 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
-// require('dotenv').config();
+require('dotenv').config();
 const path = require('path');
 
 const app = express();
